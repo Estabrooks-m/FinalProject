c432d91505140ddcd9b1cc450ab278c132dbcc05
julia.faubus <julia.faubus@gmail.com>
2024-03-23 13:58:36 -0400

    Added the new python final that will be in object oriented style (but still keeping the other file for reference)

---
+++ HuskyHurdleObjectOriented.py
@@ -0,0 +1,37 @@
+import pygame as py
+
+py.init() 
+#creates the screen with specific dimensions
+win = py.display.set_mode((1400, 1000)) 
+
+class Husky:
+
+    def __init__(self, image, background):
+        #define the image and the background with coordinates and speed
+        None
+
+    def pillars(self):
+        #use to create the pillars (using the random function)
+        None
+
+    def flap(self):
+        while True:
+            #use to create the movement when the user presses certain keys
+            None
+
+    def update(self):
+        #use to have the constant movement (falling + moving forward)
+        None
+
+class Collision(Husky):
+    def __init__(self):
+        #define necessary variables
+        None
+
+    def collisionTest(self):
+        #checks for collision
+        None
+
+    def ifCollision(self):
+        #returns a game over screen
+        None
