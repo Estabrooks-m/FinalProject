265904af915bcb456f6a5a1f8e5c0a7fcee86bb6
julia.faubus <julia.faubus@gmail.com>
2024-04-15 21:17:57 -0400

    Changed the scoring algorithm, cleaned up code, and added comments

--- HuskyHurdleObjectOriented.py
+++ HuskyHurdleObjectOriented.py
@@ -7,9 +7,7 @@ import sys
 class Husky:
     #define the image and the background with coordinates and speed
     def __init__(self, win):
-        print("Checking for initializing")
         py.init() 
-        #set caption
         py.display.set_caption("Husky Hurdle")
         #creates the "speed" that it falls
         self.falling = 0.15
@@ -17,33 +15,29 @@ class Husky:
         self.jumping = 4.5
         #creates the "speed" in the ydirection
         self.yspeed = 1
-
         self.win = win
     
         #loads the background image
         self.background = py.image.load("background.png")
-        #edits the size of the background image
 
         #outputs the background on the screen with the size of the screensize
         self.background = py.transform.smoothscale(self.background, self.win.get_size())
        
 
         
-        #loads the husky image
+        #the following two lines edits and loads the husky image
         self.husky = py.image.load("husky.png")
-        #edits the size of the husky image
         self.husky = py.transform.scale(self.husky, (150, 125)) 
         #sets the height to the second element in the string that self.win.get_size() contains
         height = self.win.get_size()[1]
         #get_rect returns the coordinates of the husky (1000 is the height)
         self.HuskyCoord = self.husky.get_rect(center=(100, height  // 2))
         
-
+#for pillar generation and movement
 class Pillar():
+    #initializes necessary variables
     def __init__(self, win):
-        #initializes an empty list for the pillars
         self.pillars = []
-        #initializes win
         self.win = win
 
     def generation(self):
@@ -111,65 +105,66 @@ class Movement():
             self.win.blit(self.HuskyInst.background, (0, 0))
 
             for event in py.event.get():
-                
+                #checks if a key has been clicked
                 if event.type == py.KEYDOWN:
+                    #if the key is q the program quits
                     if event.key == py.K_q:
                         py.display.quit()
                         sys.exit()
+                    #controls when the husky humps
                     if event.key == py.K_SPACE:
                         self.HuskyInst.yspeed = -self.HuskyInst.jumping
                     if event.key == py.K_p:
-                      
+                      #maintains the while loop when the user pauses
                       waiting = True
                       self.font = py.font.Font(None, 50)
 
                       while waiting:
-                        print("wait")
+                        #creates a variable for the time the user pauses the game
                         startingTime = py.time.get_ticks()
+                        #creates text for the user to know whats happening
                         pausedText = self.font.render('PAUSED', True, (0, 0, 160))
                         pausedScreen = self.font.render('(CLICK R to replay, Q to quit, P to unpause)', True, (0, 0, 160))
                         self.win.blit(pausedScreen, dest=(400,420))
                         self.win.blit(pausedText, dest=(640,390))
+                        #updates the screen after the text has been blit
                         py.display.update()
-                        print(f"starting pause = {self.pausedTime}")
-                        print(f"starting time = {startingTime}")
                         typed = py.event.wait() 
                        
-
+                        #if p is pressed, game unpauses
                         if typed.type == py.KEYDOWN:
                             if typed.key == py.K_p:
                                 print("second p pressed")
                                 waiting = False
-
+                            #if q is pressed, game quits
                             elif typed.key == py.K_q:
                                 print("q pressed")
                                 py.display.quit()
                                 sys.exit()
-
+                            #if r is pressed, game goes to collision screen
                             elif typed.key == py.K_r:
                                     print("r pressed")
                                     waiting = False
                                     self.CollisionInst.ifCollision() == True
                                     #if they want to play again, call reset method
                                     self.reset()
-                            elif typed.key == py.K_q:
-                                    waiting = False
-                                    self.going = False
+                            #gets the time the user unpauses
                             endingTime = py.time.get_ticks()
+                            #creates a variable with the amount of time paused (used for pillar generation)
                             self.pausedTime += endingTime - startingTime
-                            
-                            
-                            print(f"ending pause = {self.pausedTime}")
-                            print(f"ending time = {endingTime}")
 
 
 
 
+            #generates the pillars using time 
+            #self.time accounts for if the user has played a game before (restarts the clock in a way)
+            #self.pausedTime holds the amount of time the user was paused during that round
+            #self.pillarTime increases the value needed to be met for a pillar to generate (allows time to pass)
             if py.time.get_ticks() - self.time - self.pausedTime > self.pillarTime:
                 self.pillars.append(self.PillarInst.generation())
                 self.pillarTime += 4000
                     
-                
+            #ensures the list of pillars doesn't get too long
             if len(self.pillars) > 4:
                 self.pillars.pop(0)
          
@@ -189,8 +184,6 @@ class Movement():
             self.HuskyInst.HuskyCoord.centery += self.HuskyInst.yspeed
 
 
-
-
             #checks if there is a collision
             if self.CollisionInst.collisionTest(self.HuskyInst.HuskyCoord, self.pillars):
                 #if collision, returns lose screen + asks user if they want to play again
@@ -199,15 +192,14 @@ class Movement():
                     self.reset()
 
             #resets the husky if it falls off the screen
-            """if self.HuskyInst.HuskyCoord.y == 1500:
+            if self.HuskyInst.HuskyCoord.y > 10000:
                 if self.CollisionInst.ifCollision():
-                    self.reset()"""
+                    self.reset()
 
             #draws the husky image
             self.win.blit(self.HuskyInst.husky, self.HuskyInst.HuskyCoord)
-            #calls the draw method from the pillar class
+            #following two lines calls the draw and move methods from the pillar class
             self.PillarInst.draw(self.pillars)
-            #calls the move method from the pillar class
             self.PillarInst.move(self.pillars, self.pillarTime )
             #updates the display
             py.display.update()
@@ -216,37 +208,44 @@ class Movement():
 
 class Collision():
     def __init__(self, win):
-        #gets necessary variables
         #loads the lose screen image
         self.loseScreen = py.image.load("LoseScreen.png")
-        #edits the size of the lose screen image
-        #self.loseScreen = py.transform.scale(self.loseScreen, (1400, 1000)) 
         self.win = win
+        #edits the size of the lose screen image
         self.loseScreen = py.transform.smoothscale(self.loseScreen, self.win.get_size())
         
         self.PlayerScore = 0
         self.font = py.font.Font(None, 50)
+        self.passedPillars = []
+
+
 
     #checks for collision
     def collisionTest(self, HuskyCoord, PillarList):
+        
         for i in PillarList:
-            #if the x value of the pillar is 584 add to the score
-            if i[0].x in range(581, 587):
-                self.PlayerScore += 1
- 
+            #updates the score if the pillar hasn't passed yet and the pillar is less than the x coordinates of the husky
+            if i not in self.passedPillars:
+                if HuskyCoord.x > i[0].x:
+                    self.PlayerScore += 1
+                    self.passedPillars.append(i)
 
+ 
+            #blits the users score + details for how to pause and quit
             score = self.font.render(f'Score: {self.PlayerScore}', True, (0, 0, 160))
             self.win.blit(score, dest=(50, 50))
             pause = self.font.render(f"Press P to pause", True, (0, 0, 160))
             self.win.blit(pause, dest=(50, 80))
             quit = self.font.render(f"Press Q to quit", True, (0, 0, 160))
             self.win.blit(quit, dest=(50, 110))
+            #checks for collision
             for j in i:
                 if j.colliderect(HuskyCoord):
                     return True
         
     #method for when there is a collision 
     def ifCollision(self):
+        #calls the lose screen method
         self.LoseScreen()
         text = self.font.render(' Collision! Would you like to play again?', True, (0, 112, 200))
         moretext = self.font.render('(CLICK R to play, Q to quit)', True, (0, 112, 200))
@@ -259,7 +258,7 @@ class Collision():
 
         #the while loop ensure that its only quit or space being picked (it wont respond to the other keys)
         while True:
-            #waits for an event from the user (lqqqike clicking a key)
+        #waits for an event from the user (like clicking a key)
          for event in py.event.get():
             if event.type == py.KEYDOWN:
                 if event.key == py.K_q:
@@ -276,13 +275,17 @@ class Collision():
 
 #use for calling necessary methods + creating instances
 def main():
-    
+    #creates the screen
     win = py.display.set_mode(flags=py.FULLSCREEN)
-
+    #creates a variable for the clock
     clock = py.time.Clock()
+    #creates an instance of the husky class, passing it win
     HuskyInstance = Husky(win)
+    #same as previous line but for the pillar class
     PillarInstance = Pillar(win)
+    #creates an instance of the movement instance, passing it the other instances, win, and clock
     MovementInstance = Movement(HuskyInstance, PillarInstance, win, clock)
+    #calls the constant method from the Movement class
     MovementInstance.constant()
     
 
